73ccb856be6015a0c75e308849c6c0b734a634fe
RaZZoRRR <46856448+RaZZoRRR@users.noreply.github.com>
2026-09-07 16:36:42 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -106,7 +106,7 @@ async function init() {
                 </td>
 
                 <td class="px-3 py-4 text-sm text-gray-500 dark:text-gray-400">
-                    -
+                    ${player.status || "Loading..."}
                 </td>
 
                 <td class="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-6">
